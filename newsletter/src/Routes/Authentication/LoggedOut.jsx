f6098154669463bb4803e30cffd0b48826252b2f
Delljones <dell_jones@outlook.com>
2022-10-02 22:26:37 +1000

    Upload

---
+++ newsletter/src/Routes/Authentication/LoggedOut.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function LoggedOut () {
+    return (
+        <div>
+            <h1> Thanks You have been Successfully Logged Out </h1>
+            <h2> Have a nice day! </h2>
+        </div>
+    )
+}
+export default LoggedOut
---
+++ newsletter/src/Routes/Header/Searchbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import "./searchbar.css"
+import {Link, Outlet} from "react-router-dom";
+import '../Post/Post'
+import '../Authentication/Login'
+
+function Searchbar() {
+    return (
+        <div>
+            <Outlet/>
+            <div class="searchbar">
+                <form method="POST">
+                    <label for="searchbar"> DEV @ Deakin.com </label>
+                    <input type="search" id="search" class="form-control" name="search" placeholder="  Search"
+                           required="required"/>
+                    <Link to="/" class="searchbutton" type="submit">Home</Link>
+                    <Link to='/post' class="searchbutton" type="submit">Post</Link>
+                    <Link to="/login" class="searchbutton" type="submit">Log In</Link>
+                    <Link to="/loggedout" class="searchbutton" type="submit">Logout</Link>
+                    <Link to="/newsletter" class="searchbutton" type="submit">Contact</Link>
+                </form>
+            </div>
+        </div>
+    )
+}
+export default Searchbar
