88b74e52c107a971ec021400a13dea1a17b18e51
Delljones <dell_jones@outlook.com>
2022-10-02 22:49:43 +1000

    Upload

--- newsletter/src/Routes/Newsletter/Newsletter.jsx
+++ newsletter/src/Routes/Newsletter/Newsletter.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 
 function Newsletter() {
-
     return (
         <div className="newsletter">
-
             <h1> This is Newsletter </h1>
-
+            <h2>Subscribe to Our Website</h2>
+            <form>
+                <label> SIGN UP FOR OUR DAILY INSIDER! <label/>
+                    <input name="email" placeholder="Email" required="required"/>
+                    <button type="submit">Submit</button>
+                </label>
+            </form>
         </div>
     )
 }
+
 export default Newsletter;
