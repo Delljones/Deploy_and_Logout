686d5767e44b074a00968d7a681251519e41d4f5
Delljones <dell_jones@outlook.com>
2022-10-02 22:23:14 +1000

    Upload

---
+++ newsletter/src/Routes/Newsletter/Newsletter.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Newsletter() {
+
+    return (
+        <div className="newsletter">
+
+            <h1> This is Newsletter </h1>
+
+        </div>
+    )
+}
+export default Newsletter;
